1629f78d0e79238e3d770edd9aeb198d0aec7f04
joji-fuck <logvmaks@gmail.com>
2026-05-28 13:22:05 +0500

    fix: header

--- resources/views/layout/section/header.blade.php
+++ resources/views/layout/section/header.blade.php
@@ -1,12 +1,12 @@
 <header class="site-header">
     <a href="{{ route('home') }}" class="site-header__logo">
-        <img src="{{ asset('images/VIZhN_NADP.png') }}" alt="ВИЖН">
+        <img src="{{ asset('images/VIZhN_NADP.svg') }}" alt="ВИЖН">
     </a>
     <div class="site-header__decor" aria-label="Журнал о Тюменском кино"></div>
 
     <style>
         .site-header__decor{
-            background-image: url('{{asset('images/zavitushki.png')}}');
+            background-image: url('{{asset('images/zavitushki.svg')}}');
         }
     </style>
 </header>
