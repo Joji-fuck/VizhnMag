b6c6d37a1d92fd695df39d4264a773f994958541
joji-fuck <logvmaks@gmail.com>
2026-06-01 15:27:08 +0500

    fix: catalog movie

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -2,32 +2,9 @@
 
 @section('content')
     <div class="masonry-movie-grid">
-        {{-- КОЛОНКА 1 --}}
-        <div class="masonry-movie-column">
-            @foreach($col1 as $post)
-                @include('layout.section.movieCard', ['post' => $post])
-            @endforeach
-        </div>
-
-        {{-- КОЛОНКА 2 --}}
-        <div class="masonry-movie-column">
-            @foreach($col2 as $post)
-                @include('layout.section.movieCard', ['post' => $post])
-            @endforeach
-        </div>
-
-        {{-- КОЛОНКА 3 --}}
-        <div class="masonry-movie-column">
-            @foreach($col3 as $post)
-                @include('layout.section.movieCard', ['post' => $post])
-            @endforeach
-        </div>
-
-        {{-- КОЛОНКА 4 --}}
-        <div class="masonry-movie-column">
-            @foreach($col4 as $post)
-                @include('layout.section.movieCard', ['post' => $post])
-            @endforeach
-        </div>
+        @foreach($posts as $post)
+            @include('layout.section.movieCard', ['post' => $post])
+        @endforeach
     </div>
+
 @endsection
