9fbdf299ab6cbfa4388fe08c97b5d543f7e3b7eb
joji-fuck <logvmaks@gmail.com>
2026-06-11 17:32:37 +0500

    hotfix logo

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -5,7 +5,7 @@
         <div style="max-width: 820px">
             {{-- Заголовок и мета-данные --}}
             <div class="article-header">
-                <h1>{{ $post->title }}</h1>
+                <h1 class="related-news-title">{{ $post->title }}</h1>
                 <div class="article-date">
                     <span class="article-category">{{ $post->category->title ?? 'Новости' }} /</span>
                     {{ $post->created_at->locale('ru')->translatedFormat('d M') }}
@@ -30,7 +30,7 @@
 
         <div class="related-news">
             <div class="related-news-wrap">
-                <h3 class="mb-4">Читайте также:</h3>
+                <h3 class="mb-4 related-news-title">Читайте также:</h3>
                 <div class="related-grid">
                     @foreach($relatedNews as $item)
                         <div class="related-item">
